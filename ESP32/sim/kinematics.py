45c7171075455859d03c4a4d860e12b73c59fb8d
Christian Krenn <christian.krenn@aviloo.com>
2024-12-17 16:16:54 +0100

    kinematics.py: some refactoring

--- ESP32/sim/kinematics.py
+++ ESP32/sim/kinematics.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 
-def fit(x, y, max_order, max_err_ratio=0.01):
+def fit_polynomial(x, y, max_order, max_err_ratio=0.01):
     num_samples = len(x)
     y_mean = np.mean(y)
     for order in range(0, max_order+1):
@@ -24,7 +24,7 @@ l_sled_stroke = 100.0       # sled stroke (mm)
 
 samples_per_mm = 10.0
 max_order = 4
-max_err = 0.005
+max_err = 0.001
 
 data = {
     'l_sled': [],
@@ -33,28 +33,31 @@ data = {
 }
 
 for i in range(int(l_sled_stroke * samples_per_mm) + 1):
-    l_sled = float(i) / samples_per_mm
-    l_pivot_sled_x = l_pivot_sled_x_min + l_sled
-    phi_pivot_sled = math.atan2(l_pivot_sled_y, l_pivot_sled_x) # angle of the connecting line between pivot and sled connection point (from horizontal)
+    l_sled = float(i) / samples_per_mm # sled position within stroke
+    l_pivot_sled_x = l_pivot_sled_x_min + l_sled # horizontal position of the sled connection point relative to the pivot/origin
+    phi_pivot_sled = math.atan2(l_pivot_sled_y, l_pivot_sled_x) # angle of the connecting line between pivot and sled connection point (from horizontal axis)
     l_pivot_sled = math.sqrt(l_pivot_sled_y ** 2 + l_pivot_sled_x ** 2) # length of the connecting line between pivot and sled connection point
     phi_link_pivot_sled = math.acos((l_link ** 2 - l_pivot_link ** 2 - l_pivot_sled ** 2) / (l_pivot_link * l_pivot_sled * -2)) # angle between the connecting line between pivot and link and the connecting line between pivot and sled connection point
     phi_link_sled_pivot = math.acos((l_pivot_link ** 2 - l_link ** 2 - l_pivot_sled ** 2) / (l_link * l_pivot_sled * -2)) # angle between the link and the connecting line between pivot and sled connection point
     phi_pivot_link_sled = math.pi - phi_link_pivot_sled - phi_link_sled_pivot # third angle of the triangle between pivot, link connection point and sled connection point
     phi_ped_vert = (math.pi / 2.0) - (phi_link_pivot_sled + phi_pivot_sled) # pedal angle (from vertical axis)
-    phi_foot_link = (math.pi / 2.0) - phi_pivot_link_sled # angle between pedal normal and link
-    r_force_link_foot = 1.0 / math.cos(phi_foot_link) * l_pivot_link / l_pivot_foot # ratio between load cell measurement (in link direction) and foot force (tangential to pedal)
-    l_foot = phi_ped_vert * l_pivot_foot # foot position as an arc length relative to vertical
+    phi_foot = -phi_ped_vert # foot angle (pedal normal, from horizontal axis)
+    phi_link = -(math.pi / 2.0) - phi_ped_vert + phi_pivot_link_sled # link angle (from horizontal axis)
+    phi_foot_link = phi_foot - phi_link # angle between foot force direction and link
+    r_force_foot_link = (l_pivot_foot / l_pivot_link) * math.cos(phi_foot_link) # ratio between foot force and load cell measurement
+    r_force_link_foot = 1.0 / r_force_foot_link # ratio between load cell measurement and foot force
+    l_foot = phi_ped_vert * l_pivot_foot # foot position as an arc length relative to vertical axis
     data['l_sled'].append(l_sled)
     data['r_force_link_foot'].append(r_force_link_foot)
     data['l_foot'].append(l_foot)
 
 print(f'fitting r_force_link_foot over l_sled...')
-coeff_l_sled_to_r_force = fit(data['l_sled'], data['r_force_link_foot'], max_order, max_err)
+coeff_l_sled_to_r_force = fit_polynomial(data['l_sled'], data['r_force_link_foot'], max_order, max_err)
 print(f'coefficients: {str(coeff_l_sled_to_r_force).replace('.',',')}')
 
 print()
 
 print(f'fitting l_sled over l_foot...')
-coeff_l_foot_to_l_sled = fit(data['l_foot'], data['l_sled'], max_order, max_err)
+coeff_l_foot_to_l_sled = fit_polynomial(data['l_foot'], data['l_sled'], max_order, max_err)
 print(f'coefficients: {str(coeff_l_foot_to_l_sled).replace('.',',')}')
 
